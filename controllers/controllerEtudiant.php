e0b3f34a39a7dd0e625537d4deb192abc7539c76
crybaby050 <benthiam750@gmail.com>
2025-10-25 13:38:52 +0000

    mise a jour 1 de controllerEtudiant

--- controllers/controllerEtudiant.php
+++ controllers/controllerEtudiant.php
@@ -1,2 +1,16 @@
 <?php
 require_once 'models/modelEtudiant.php';
+function liste_etudiant(){
+    $classe = findAllClasse();
+    $error = '';
+    if (isset($_REQUEST['id'])) {
+        $id = intval($_REQUEST['id']);
+        delEtudiantById($id);
+    }
+    $etude = findAllEtudiant();
+    if (isset($_REQUEST['nivfil'])) {
+        $val = trim($_REQUEST['niv']);
+        $etude = filteredByClasse($val, $etude, $classe);
+        require_once('liste.php');
+    }
+}

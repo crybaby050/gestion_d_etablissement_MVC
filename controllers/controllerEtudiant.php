1fcfddf1dfa3774d6dc54ff4af87ca14f629c106
crybaby050 <benthiam750@gmail.com>
2025-10-25 16:09:12 +0000

    modification de certain element de controlleEtudiant, ajout de controllerUser et modelUser dans le but de tester les affichage des pages login et liste

--- controllers/controllerEtudiant.php
+++ controllers/controllerEtudiant.php
@@ -1,16 +1,20 @@
 <?php
 require_once 'models/modelEtudiant.php';
 function liste_etudiant(){
-    $classe = findAllClasse();
     $error = '';
+    //partie supression de l'etudiant a partir de l'id recuperer
+    $classe = findAllClasse();
     if (isset($_REQUEST['id'])) {
         $id = intval($_REQUEST['id']);
         delEtudiantById($id);
+        header("Location:".WEBROOT."index.php?page=liste_etudiant");
     }
+    //partie filtrage de l'etudiant
     $etude = findAllEtudiant();
     if (isset($_REQUEST['nivfil'])) {
         $val = trim($_REQUEST['niv']);
         $etude = filteredByClasse($val, $etude, $classe);
-        require_once('liste.php');
+        // require_once('liste.php');
     }
+    require_once 'view/etudiant/liste.php';
 }

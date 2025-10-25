155ea63ab1a79b15e334872200d4773019dd231f
crybaby050 <benthiam750@gmail.com>
2025-10-25 02:57:14 +0000

    premier commit de la journée j'ai oublier ce que j'avais fais hier mais je crois que c'est en rapport avec les modelEtudiants

--- models/modelEtudiant.php
+++ models/modelEtudiant.php
@@ -1,3 +1,26 @@
 <?php
 //test du model etudiants
 require_once 'models/modelData.php';
+function findAllEtudiant():array{
+    $datas=jsonToArray();
+    return $datas['etudiant'];
+}
+function detailById($id): array {
+    $tab = findAllEtudiant();
+    foreach ($tab as $v) {
+        if ($v['id'] == $id) {
+            return $v;
+        }
+    }
+    return [];
+}
+function modifierById($modif): void {
+    $datas = jsonToArray();
+    foreach ($datas['etudiant'] as $index => $mod) {
+        if ((int)$mod['id'] === (int)$modif['id']) {
+            $datas['etudiant'][$index] = $modif;
+            arrayToJson($datas);
+            return; // sortie dès que modif faite
+        }
+    }
+}

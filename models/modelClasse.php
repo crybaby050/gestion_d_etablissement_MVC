bdec6efe60fb07ca987a23fe3faff0fa393c64ec
crybaby050 <benthiam750@gmail.com>
2025-10-28 13:02:44 +0000

    correction de bug car je vien de decouvrir que des fonction que j'avais creer pour modelEtudiant et dont j'ai encore besoin sur modelClasse, ce n'est pas necessaire que cette fontion soit present sur les deux model  a cause require once donc je vais enlever l'un d'eux et voir ce que cela donne

--- models/modelClasse.php
+++ models/modelClasse.php
@@ -2,10 +2,10 @@
 //creation du model classe
 require_once 'models/modelData.php';
 //recuperation de donnée des classes
-function findAllClasse():array{
-    $datas=jsonToArray();
-    return $datas['classe'];
-}
+// function findAllClasse():array{
+//     $datas=jsonToArray();
+//     return $datas['classe'];
+// }
 //recuperation des données de niveau et de filiere pour le filtrages des classes
 function findAllFilliere():array{
     $datas=jsonToArray();
@@ -16,11 +16,11 @@ function findAllNiveau():array{
     return $datas['niveau'];
 }
 //fonction pour determiner si le tableau est vide
-function presence($tab):void{
-    if(count($tab)==0){
-        echo "<h3> Auncun element enregistrer</h3>";
-    }
-}
+// function presence($tab):void{
+//     if(count($tab)==0){
+//         echo "<h3> Auncun element enregistrer</h3>";
+//     }
+// }
 //fonction pour les details des classes
 function detailClasseById($id): array {
     $tab = findAllClasse();
